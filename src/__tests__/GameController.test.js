ac7e08a8e34fe769c5a0ec91a6955ea7742809cf
Юлия Смирнова <dolphin_99@mail.ru>
2023-04-01 15:58:22 +0300

    GameState and competitor strategy are not done yet

--- src/__tests__/GameController.test.js
+++ src/__tests__/GameController.test.js
@@ -1,8 +1,11 @@
 import { generateTeam, characterGenerator } from '../js/generators';
 import Character from '../js/Character';
+import Bowman from '../js/characters/Bowman';
 import Daemon from '../js/characters/Daemon';
-import Vampire from '../js/characters/Vampire';
+import Magician from '../js/characters/Magician';
+import Swordsman from '../js/characters/Swordsman';
 import Undead from '../js/characters/Undead';
+import Vampire from '../js/characters/Vampire';
 import GameController from '../js/GameController';
 import GamePlay from '../js/GamePlay';
 import GameStateService from '../js/GameStateService';
@@ -54,4 +57,38 @@ test('should show all characteristics when mouseover', () => {
   const gameCtrl = new GameController(gamePlay, stateService);
   const character = new Daemon(3);
   expect(gameCtrl.showCharacterInfo({ character, position: 3 })).toBe('\u{1F396} 3 \u2694 10 \u{1F6E1} 10 \u2764 50');
+});
+
+test('should check is Bowman allowed to move and attack', () => {
+  const gamePlay = new GamePlay();
+  const stateService = new GameStateService({});
+  const gameCtrl = new GameController(gamePlay, stateService);
+  gamePlay.playerTypes = [Bowman, Swordsman, Magician];
+  gamePlay.competitorTypes = [Daemon, Vampire, Undead];
+  const bowman = { character: new Bowman(2), position: 45 };
+  const magician = { character: new Magician(3), position: 46 };
+  const swordsman = { character: new Swordsman(1), position: 55 };
+  const daemon = { character: new Daemon(1), position: 14 };
+  const undead = { character: new Undead(3), position: 18 };
+  const vampire = { character: new Vampire(3), position: 47 };
+  gameCtrl.positions = [bowman, magician, swordsman, daemon, undead, vampire];
+  expect(gameCtrl.checkMovement(46, 45, bowman, () => 'allowed', () => 'not allowed')).toBe('not allowed');
+  expect(gameCtrl.checkMovement(44, 45, bowman, () => 'allowed', () => 'not allowed')).toBe('allowed');
+  expect(gameCtrl.checkAttack(47, 45, bowman, () => 'allowed', () => 'not allowed')).toBe('allowed');
+  expect(gameCtrl.checkAttack(18, 45, bowman, () => 'allowed', () => 'not allowed')).toBe('not allowed');
+
+  expect(gameCtrl.checkMovement(53, 46, magician, () => 'allowed', () => 'not allowed')).toBe('allowed');
+  expect(gameCtrl.checkAttack(14, 46, magician, () => 'allowed', () => 'not allowed')).toBe('allowed');
+
+  expect(gameCtrl.checkMovement(19, 55, swordsman, () => 'allowed', () => 'not allowed')).toBe('allowed');
+  expect(gameCtrl.checkAttack(47, 55, swordsman, () => 'allowed', () => 'not allowed')).toBe('allowed');
+
+  expect(gameCtrl.checkMovement(19, 14, daemon, () => 'allowed', () => 'not allowed')).toBe('not allowed');
+  expect(gameCtrl.checkAttack(47, 14, daemon, () => 'allowed', () => 'not allowed')).toBe('not allowed');
+
+  expect(gameCtrl.checkMovement(22, 18, undead, () => 'allowed', () => 'not allowed')).toBe('allowed');
+  expect(gameCtrl.checkAttack(19, 18, undead, () => 'allowed', () => 'not allowed')).toBe('not allowed');
+
+  expect(gameCtrl.checkMovement(61, 47, vampire, () => 'allowed', () => 'not allowed')).toBe('allowed');
+  expect(gameCtrl.checkAttack(45, 47, vampire, () => 'allowed', () => 'not allowed')).toBe('allowed');
 });
--- src/js/GameController.js
+++ src/js/GameController.js
@@ -58,40 +58,134 @@ export default class GameController {
   }
 
   onCellClick (index) {
-    const alreadyActive = this.gamePlay.cells.find(cell => cell.classList.contains('selected'));
+    const alreadyActive = this.gamePlay.cells.find(cell => cell.classList.contains('selected-yellow'));
     const characterInCell = this.positions.find(obj => obj.position === index);
+    const activePosition = this.gamePlay.cells.indexOf(alreadyActive);
+    const activeCharacter = this.positions.find(obj => obj.position === activePosition);
 
     if (alreadyActive) {
       this.gamePlay.deselectCell(this.gamePlay.cells.indexOf(alreadyActive));
     }
 
-    if (alreadyActive && !characterInCell) {
-      const activePosition = this.gamePlay.cells.indexOf(alreadyActive);
-      const position = this.positions.find(obj => obj.position === activePosition);
-      position.position = index;
-      this.gamePlay.redrawPositions(this.positions);
-    }
+    if (alreadyActive && characterInCell) {
+      const isCompetotorInCell = this.competitorTypes.some(player => player.name === characterInCell.character.constructor.name);
+      if (isCompetotorInCell) {
+        const doDamage = () => {
+          const damage = Math.max(activeCharacter.character.attack - characterInCell.character.defence, activeCharacter.character.attack * 0.1);
+          const showDamage = this.gamePlay.showDamage(index, damage);
+          showDamage.then(() => {
+            characterInCell.character.health -= damage;
+            this.gamePlay.cells[index].classList.remove('selected-red');
+            this.gamePlay.setCursor(cursors.auto);
 
-    if (characterInCell && (this.playerTypes.some((type) => type.name === characterInCell.character.constructor.name))) {
-      this.gamePlay.selectCell(index);
-    }
-    if (characterInCell && !alreadyActive && !(this.playerTypes.some((type) => type.name === characterInCell.character.constructor.name))) {
-      GamePlay.showError('Сейчас Ваш ход');
-    }
-  }
+            if (characterInCell.character.health < 1) {
+              const elToDelete = this.positions.indexOf(characterInCell);
+              this.positions.splice(elToDelete, 1);
+            }
+            this.gamePlay.redrawPositions(this.positions);
+            if (!this.positions.some((el) => this.competitorTypes.some((type) => type.name === el.character.constructor.name))) {
+              this.positions.forEach((el) => {
+                const attackAfter = Math.max(el.character.attack, el.character.attack * (80 + el.character.health) / 100);
+                el.character.attack = attackAfter;
 
-  toMove (player) {
-    const whereToMove = (step) => {
-      const activePosition = this.gamePlay.cells.indexOf(player);
-      const allowedPositions = [];
-      for (let i = activePosition; (i > -1) && (i < this.gamePlay.boardSize - 1); i++) {
-        allowedPositions.push(i);
+                const defenceAfter = Math.max(el.character.defence, el.character.defence * (80 + el.character.health) / 100);
+                el.character.defence = defenceAfter;
+
+                el.character.health += 80;
+                if (el.character.health > 100) {
+                  el.character.health = 100;
+                };
+              });
+            }
+          });
+        };
+        this.checkAttack(index, activePosition, activeCharacter, doDamage, () => alert('Противник вне зоны Вашей атаки!'));
       }
     };
 
-    if (player === Bowman) {
-      whereToMove(2);
+    if (alreadyActive && !characterInCell) {
+      const moveCharacter = () => {
+        activeCharacter.position = index;
+        this.gamePlay.redrawPositions(this.positions);
+        this.gamePlay.deselectCell(index);
+      };
+
+      this.checkMovement(index, activePosition, activeCharacter, moveCharacter, () => alert('Сюда нельзя сделать ход!'));
+    };
+
+    if (characterInCell && (this.playerTypes.some((type) => type.name === characterInCell.character.constructor.name))) {
+      this.gamePlay.selectCell(index);
+    };
+
+    if (characterInCell && !alreadyActive && !(this.playerTypes.some((type) => type.name === characterInCell.character.constructor.name))) {
+      GamePlay.showError('Сейчас Ваш ход');
+    };
+  }
+
+  toMove (idCursor, idActive, size = this.gamePlay.boardSize) {
+    let result;
+    const diff = Math.abs(idCursor - idActive);
+    const mod = idCursor % size;
+    const diagDiffKoef = Math.abs(
+      Math.floor(idCursor / size) - Math.floor(idActive / size)
+    );
+    // vertical
+    if (mod - (idActive % size) === 0) {
+      result = diff / size;
     }
+    // horisontal
+    if (Math.floor(idCursor / size) === Math.floor(idActive / size)) {
+      result = diff;
+    }
+    // left diagonal
+    if (diff === (size - 1) * diagDiffKoef) {
+      result = diff / (size - 1);
+    }
+    // right diagonal
+    if (diff === (size + 1) * diagDiffKoef) {
+      result = diff / (size + 1);
+    }
+    return result;
+  }
+
+  checkMovement (index, activePosition, activeCharacter, func, errorFunc) {
+    const isAllowedToMove = this.toMove(index, activePosition);
+    const isEmptyCell = this.positions.find(obj => obj.position === index);
+    const activeType = activeCharacter.character.type;
+    if (isAllowedToMove) {
+      if (((activeType === 'swordsman') || (activeType === 'undead')) && (isAllowedToMove < 5) && (isEmptyCell === undefined)) {
+        return func();
+      };
+
+      if (((activeType === 'bowman') || (activeType === 'vampire')) && (isAllowedToMove < 3) && (isEmptyCell === undefined)) {
+        return func();
+      };
+
+      if (((activeType === 'magician') || (activeType === 'daemon')) && (isAllowedToMove < 2) && (isEmptyCell === undefined)) {
+        return func();
+      }
+    }
+    return errorFunc();
+  }
+
+  checkAttack (index, activePosition, activeCharacter, func, errorFunc) {
+    const isAllowedToAttack = this.toMove(index, activePosition);
+    const isEmptyCell = this.positions.find(obj => obj.position === index);
+    const activeType = activeCharacter.character.type;
+    if (isAllowedToAttack) {
+      if (((activeType === 'swordsman') || (activeType === 'undead')) && (isAllowedToAttack < 2) && (isEmptyCell !== undefined)) {
+        return func();
+      };
+
+      if (((activeType === 'bowman') || (activeType === 'vampire')) && (isAllowedToAttack < 3) && (isEmptyCell !== undefined)) {
+        return func();
+      };
+
+      if (((activeType === 'magician') || (activeType === 'daemon')) && (isAllowedToAttack < 5) && (isEmptyCell !== undefined)) {
+        return func();
+      }
+    }
+    return errorFunc();
   }
 
   addClickListener () {
@@ -100,15 +194,49 @@ export default class GameController {
 
   onCellEnter (index) {
     const characterInCell = this.positions.find(obj => obj.position === index);
+    const alreadyActive = this.gamePlay.cells.find(cell => cell.classList.contains('selected-yellow'));
+    const activePosition = this.gamePlay.cells.indexOf(alreadyActive);
+    const activeCharacter = this.positions.find(obj => obj.position === activePosition);
+
     if (characterInCell) {
-      const alreadyActive = this.gamePlay.cells.find(cell => cell.classList.contains('selected'));
+      this.gamePlay.showCellTooltip(this.showCharacterInfo(characterInCell), index);
+
       const playerChar = this.playerTypes.some((type) => type.name === characterInCell.character.constructor.name);
       if ((this.gamePlay.cells.indexOf(alreadyActive) !== characterInCell.position) && playerChar) {
         this.gamePlay.setCursor(cursors.pointer);
       }
-    }
-    if (characterInCell) {
-      this.gamePlay.showCellTooltip(this.showCharacterInfo(characterInCell), index);
+    };
+
+    if (alreadyActive) {
+      const highlightGreenCell = () => {
+        const alreadyHighlighted = this.gamePlay.cells.indexOf(this.gamePlay.cells.find(cell => cell.classList.contains('selected-green')));
+        if (alreadyHighlighted > -1) {
+          this.gamePlay.deselectCell(alreadyHighlighted);
+        }
+        this.gamePlay.selectCell(this.gamePlay.cells.indexOf(this.gamePlay.cells[index]), 'green');
+        this.gamePlay.setCursor(cursors.pointer);
+      };
+
+      this.checkMovement(index, activePosition, activeCharacter, highlightGreenCell, () => this.gamePlay.setCursor(cursors.notallowed));
+
+      if (characterInCell) {
+        if (this.playerTypes.some((type) => type.name === characterInCell.character.constructor.name)) {
+          this.gamePlay.setCursor(cursors.pointer);
+        };
+
+        const isCompetotorInCell = this.competitorTypes.some(player => player.name === characterInCell.character.constructor.name);
+        if (isCompetotorInCell) {
+          const highlightRedCell = () => {
+            const alreadyHighlighted = this.gamePlay.cells.indexOf(this.gamePlay.cells.find(cell => cell.classList.contains('selected-red')));
+            if (alreadyHighlighted > -1) {
+              this.gamePlay.deselectCell(alreadyHighlighted);
+            }
+            this.gamePlay.selectCell(this.gamePlay.cells.indexOf(this.gamePlay.cells[index]), 'red');
+            this.gamePlay.setCursor(cursors.crosshair);
+          };
+          this.checkAttack(index, activePosition, activeCharacter, highlightRedCell, () => this.gamePlay.setCursor(cursors.notallowed));
+        }
+      }
     }
   }
 
@@ -127,7 +255,9 @@ export default class GameController {
   onCellLeave (index) {
     this.gamePlay.hideCellTooltip(index);
     this.gamePlay.setCursor(cursors.auto);
-    console.log('done');
+    if (!this.gamePlay.cells[index].classList.contains('selected-yellow')) {
+      this.gamePlay.deselectCell(this.gamePlay.cells.indexOf(this.gamePlay.cells[index]));
+    }
   }
 
   addLeaveListener () {
